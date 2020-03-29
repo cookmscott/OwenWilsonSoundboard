29aede474d2894d40f664daa894987af2b8e8502
unknown <cookmscott@gmail.com>
2020-03-29 11:30:42 -0400

    First push, fully working owen soundboard

---
+++ lib/main.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:owowen_wilson/sounds.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+      ),
+      home: MyHomePage(title: 'Owen Wilson Wowboard'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  /// Owen's Faces
+  List<String> _headshots = [
+    'assets/images/owen1.png',
+    'assets/images/owen2.png',
+    'assets/images/owen3.png',
+    'assets/images/owen4.png',
+    'assets/images/owen5.png',
+    'assets/images/owen6.png',
+  ];
+
+  /// Owen's WOWs
+  final List _wows = WowSounds().wows;
+
+  /// Audio Cache
+  AudioCache audioCache;
+  AudioPlayer audioPlayer;
+  int indexIsPlaying;
+
+  @override
+  void initState() {
+    super.initState();
+    initSounds();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Text('Click a face, hear a WOW!'),
+          GridView.builder(
+            shrinkWrap: true,
+            itemCount: 15,
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisSpacing: 5,
+              mainAxisSpacing: 5,
+              crossAxisCount: 3,
+            ),
+            itemBuilder: (BuildContext context, int index) => GestureDetector(
+              child: Center(
+                child: AnimatedContainer(
+                  width: indexIsPlaying == index ? 120 : 80,
+                  height: indexIsPlaying == index ? 120 : 80,
+                  duration: Duration(milliseconds: 400),
+                  curve: Curves.bounceOut,
+                  decoration: BoxDecoration(
+                    color: indexIsPlaying == index
+                        ? Colors.white
+                        : Colors.yellow[500],
+                    borderRadius: new BorderRadius.circular(100.0),
+                    image: new DecorationImage(
+                      image: new AssetImage(_headshots[index % 5]),
+                      fit: BoxFit.fill,
+                    ),
+                    border: new Border.all(
+                        color: indexIsPlaying == index
+                            ? Colors.yellow
+                            : Colors.transparent,
+                        width: 2.0,
+                        style: BorderStyle.solid),
+                    boxShadow: [
+                      new BoxShadow(
+                        color: Colors.grey.withOpacity(0.4),
+                        offset: Offset(0, 10.0),
+                        blurRadius: 10.0,
+                      )
+                    ],
+                  ),
+                  child: Align(
+                    alignment: Alignment.bottomRight,
+                    child: Text('${index + 1}'),
+                  ),
+                ),
+              ),
+              onTap: () {
+                if (mounted) {
+                  setState(() {
+                    playSound(_wows[index]);
+                    indexIsPlaying = index;
+                  });
+                }
+              },
+            ),
+            padding: const EdgeInsets.all(20),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void initSounds() async {
+    audioPlayer = AudioPlayer();
+    audioCache = AudioCache(fixedPlayer: audioPlayer);
+    audioCache.loadAll(_wows);
+  }
+
+  void playSound(wow) async {
+    var fileName = wow;
+    if (audioPlayer.state == AudioPlayerState.PLAYING) {
+      audioPlayer.stop();
+    }
+    audioPlayer = await audioCache.play(fileName);
+  }
+
+  void stopSound(wow) {
+    audioPlayer.stop();
+  }
+}
---
+++ lib/sounds.dart
@@ -0,0 +1,20 @@
+class WowSounds {
+  List<String> _wows = [
+    'sounds/wow1.mp3',
+    'sounds/wow2.mp3',
+    'sounds/wow3.mp3',
+    'sounds/wow4.mp3',
+    'sounds/wow5.mp3',
+    'sounds/wow6.mp3',
+    'sounds/wow7.mp3',
+    'sounds/wow8.mp3',
+    'sounds/wow9.mp3',
+    'sounds/wow10.mp3',
+    'sounds/wow11.mp3',
+    'sounds/wow12.mp3',
+    'sounds/wow13.mp3',
+    'sounds/wow14.mp3',
+    'sounds/wow15.mp3',
+  ];
+  get wows => _wows;
+}
